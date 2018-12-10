83e4287adfec034a4158d37ab6bf22be3328b1e2
朱文骁 <34543176+zwx19981207@users.noreply.github.com>
2018-12-10 10:51:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import sqlite3
+import time
+
 
 GG = ['memberId', 'photoUrl', 'nick', 'gender', 'realName', 'majorName', 'gradeNumber', 'classNumber', 'type', 'fans', 'attention']
 
@@ -12,32 +14,38 @@ headers = {'token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NDQ5MjQwMjA
            'Host':'portal.328ym.com',
            'Accept-Encoding':'gzip',
            'User-Agent':'okhttp/3.12.0'}
-conn = sqlite3.connect('test.db')
+conn = sqlite3.connect('new.db')
 c = conn.cursor()
 
 
-for number in range(1,31):
+for number in range(1, 40):
 
-    data = {'groupId': '58ea09b5ec9611e8bb3c02420a183a07', 'token':'27c23fc682284428a6c8bd9cd3f246e5', 'pageNo':str(number), 'pageSize':'100'}
-    r = requests.post(url='http://portal.328ym.com/find/groupMember/memberList', headers=headers, data=data)
-    r_json=json.loads(r.text)
+    data = {'groupId': '58ea09b5ec9611e8bb3c02420a183a07', 'token': '27c23fc682284428a6c8bd9cd3f246e5', 'pageNo': str(number), 'pageSize': '100'}
+    try:
+        r = requests.post(url='http://portal.328ym.com/find/groupMember/memberList', headers=headers, data=data)
+        r_json = json.loads(r.text)
 
 
 
-    mess = r_json['data']['result']
-    for num in range(0, 100):
-        try:
-            mes = r_json['data']['result'][num]
-        except:
-            continue
-        mes2 = str(GG[0]+','+GG[1]+','+GG[2]+','+GG[3]+','+GG[4]+','+GG[5]+','+GG[6]+','+GG[7]+','+GG[8]+','+GG[9]+','+GG[10])
-        mes3 = "\""+str(mes[GG[0]])+"\""+','+"\""+str(mes[GG[1]])+"\""+','+"\""+str(mes[GG[2]])+"\""+','+"\""+str(mes[GG[3]])+"\""+','+"\""+str(mes[GG[4]])+"\""+','+"\""+str(mes[GG[5]])+"\""+','+"\""+str(mes[GG[6]])+"\""+','+"\""+str(mes[GG[7]])+"\""+','+"\""+str(mes[GG[8]])+"\""+','+"\""+str(mes[GG[9]])+"\""+','+"\""+str(mes[GG[10]])+"\""
-        submess = "INSERT INTO Message ("+mes2+") "+"VALUES ( "+mes3+" );"
-        try:
-            c.execute(submess)
-        except BaseException:
-            print(submess)
-            continue
-c.close()
+        mess = r_json['data']['result']
+        for num in range(0, 100):
+            try:
+                mes = r_json['data']['result'][num]
+            except:
+                continue
+            mes2 = str(GG[0]+','+GG[1]+','+GG[2]+','+GG[3]+','+GG[4]+','+GG[5]+','+GG[6]+','+GG[7]+','+GG[8]+','+GG[9]+','+GG[10])
+            mes3 = "\""+str(mes[GG[0]])+"\""+','+"\""+str(mes[GG[1]])+"\""+','+"\""+str(mes[GG[2]])+"\""+','+"\""+str(mes[GG[3]])+"\""+','+"\""+str(mes[GG[4]])+"\""+','+"\""+str(mes[GG[5]])+"\""+','+"\""+str(mes[GG[6]])+"\""+','+"\""+str(mes[GG[7]])+"\""+','+"\""+str(mes[GG[8]])+"\""+','+"\""+str(mes[GG[9]])+"\""+','+"\""+str(mes[GG[10]])+"\""
+            submess = "INSERT INTO newmessage ("+mes2+") "+"VALUES ( "+mes3+" );"
+            try:
+                c.execute(submess)
+                print(num*number)
+            except BaseException:
+                print(submess)
+                continue
+    except Exception:
+        print('zzZZZ!!!')
+        continue
+    # time.sleep(1)
 conn.commit()
+c.close()
 conn.close()
